dad46bea02a162b3cab1ce976e67f6f25a8813d8
suhyeon <minsoohyeon1234@gmail.com>
2023-03-18 00:40:08 +0900

    post API 구현 완료

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -3,28 +3,28 @@ const { Users } = require('../models')
 require('dotenv').config()
 
 module.exports = async (req, res, next) => {
-  const { authorization } = req.cookies
+    const { authorization } = req.cookies
 
-  const [authType, authToken] = (authorization ?? '').split(' ')
+    const [authType, authToken] = (authorization ?? '').split(' ')
 
-  if (authType !== 'Bearer' || !authToken) {
-    return res.status(401).json({
-      errorMessage: '로그인 후에 사용하세요.',
-    })
-  }
+    if (authType !== 'Bearer' || !authToken) {
+        return res.status(401).json({
+            errorMessage: '로그인 후에 사용하세요.',
+        })
+    }
 
-  try {
-    const { userId } = jwt.verify(authToken, process.env.TOKEN_KEY)
+    try {
+        const { userId } = jwt.verify(authToken, process.env.TOKEN_KEY)
 
-    const user = await Users.findOne({ where: { userId } })
+        const user = await Users.findOne({ where: { userId } })
 
-    res.locals.user = user
-    next()
-  } catch (err) {
-    console.error(err)
+        res.locals.user = user
+        next()
+    } catch (err) {
+        console.error(err)
 
-    return res.status(401).json({
-      errorMessage: '로그인 후에 사용하세요',
-    })
-  }
+        return res.status(401).json({
+            errorMessage: '로그인 후에 사용하세요',
+        })
+    }
 }
--- routes/post.router.js
+++ routes/post.router.js
@@ -1,4 +1,140 @@
 const express = require('express')
 const router = express.Router()
+const authmiddleware = require('../middlewares/auth-middleware') 
+const { Posts, Users } = require('../models')
+
+// 게시글 전체 조회 API
+router.get('/', async (req, res, next) => {
+    try {
+        const posts = await Posts.findAll({
+            raw: true,
+            attributes: ['postId', 'title', 'content', 'img', 'createdAt', 'updatedAt', 'User.nickname', 'userId'],
+            include: [
+                {
+                    model: Users,
+                    attributes: []
+                }
+            ]
+        })
+        res.json({ posts })
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({ msg: "예상하지 못한 에러가 발생하였습니다." })
+        next()
+    }
+})
+
+// 게시글 삭제 API
+router.delete('/:postId', authmiddleware, async (req, res, next) => {
+    try {
+        const { postId } = req.params
+        const user = res.locals.user
+        const post = await Posts.findOne({
+            where: { postId }
+        })
+        if (!post) {
+            return res.status(404).json({ msg: '게시글이 존재하지 않습니다.' })
+        }
+        if (post.userId != user.userId) {
+            return res.status(403).json({ msg: "게시글의 삭제 권한이 존재하지 않습니다." })
+        }
+
+        await Posts.destory({
+            where: { postId, userId: user.userId }
+        })
+        res.json({ msg: "게시글 삭제 완료" })
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({ msg: "게시글 작성에 실패하였습니다" })
+        next()
+    }
+})
+
+// 게시글 한개 조회 API
+router.get('/:postId', async (req, res, next) => {
+    try {
+        const { postId } = req.params
+        const posts = await Posts.findOne({
+            raw: true,
+            where: { postId },
+            attributes: ['postId', 'title', 'content', 'img', 'createdAt', 'updatedAt', 'User.nickname', 'userId'],
+            include: [
+                {
+                    model: Users,
+                    attributes: []
+                }
+            ]
+        })
+        if (!posts) {
+            return res.status(404).json({ msg: '게시글이 존재하지 않습니다.' })
+        }
+        
+        res.json({ posts })
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({ msg: "예상하지 못한 에러가 발생하였습니다." })
+        next()
+    }
+})
+
+// 게시글 수정 API
+router.patch('/:postId', authmiddleware,async (req, res, next) => {
+    try {
+        const { postId } = req.params
+        const { title, img, content } = req.body
+        
+        const user = res.locals.user
+        const post = await Posts.findOne({ where: { postId } })
+        
+        if (!post) {
+            return res.status(403).json({
+                msg : "게시글 수정의 권한이 존재하지 않습니다."
+            })
+        }
+        if (post.userId != user.userId) {
+            return res.status(403).json({ msg: "게시글의 수정 권한이 존재하지 않습니다." })
+        }
+        if (!title || !content) {
+            return res.status(412).json({ msg: "데이터 형식이 올바르지 않습니다." })
+        }
+        await Posts.update({
+            title,
+            img,
+            content,
+        },{where: {postId}})
+
+        res.json({ msg: "게시글 수정 완료" })
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({ msg: "예상하지 못한 에러가 발생하였습니다." })
+        next()
+    }
+
+})
+
+// 게시글 작성 API
+router.post('/', authmiddleware,async (req, res, next) => {
+    try {
+        const { title, img, content } = req.body
+        const user = res.locals.user
+
+        if (!title || !content) {
+            return res.status(412).json({ msg: "데이터 형식이 올바르지 않습니다." })
+        }
+        await Posts.create({
+            title,
+            nickname: user.nickname,
+            img,
+            content,
+            userId: user.userId
+        })
+        res.json({ msg: "게시글 작성 완료" })
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({ msg: "예상하지 못한 에러가 발생하였습니다." })
+        next()
+    }
+
+})
 
 module.exports = router

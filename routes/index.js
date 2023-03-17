7f49e87f3d101a8698ffac070cce72f8b5be1d57
wndudtjd <dudtjdwn1575@naver.com>
2023-03-17 18:16:05 +0900

    로그인 회원가입 시작

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+const loginRouter = require('./login.router')
+const signupRouter = require('./signup.router')
 
+router.use('/auth', [loginRouter, signupRouter])
 
 module.exports = router

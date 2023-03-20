389e7b008b2f7314ec92800e4bef3815a406e346
suhyeon <minsoohyeon1234@gmail.com>
2023-03-20 13:25:52 +0900

    app.js cors use  위치 변경

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
 const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
@@ -6,6 +7,13 @@ const app = express()
 const port = 3000
 const globalRouter = require('./routes')
 
+app.use(
+    cors({
+        origin: '*',
+        credentials: true,
+        optionsSuccessStatus: 200,
+    })
+)
 app.use(express.json())
 app.use(cookieParser())
 app.use(morgan('dev'))
@@ -15,15 +23,6 @@ app.use(express.static('public'))
 
 app.use('/api', globalRouter)
 
-// cors 미들웨어 사용
-const cors = require('cors')
-app.use(cors())
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Headers', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    next()
-})
 
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
